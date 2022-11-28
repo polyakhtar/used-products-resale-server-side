f5973c0297310ba28162213a5bca282b0cc82a8a
polyakhtar <poly09876pm@gmail.com>
2022-11-28 09:43:46 +0600

    users api set

--- index.js
+++ index.js
@@ -13,7 +13,6 @@ app.use(cors())
 app.use(express.json())
 
 
-
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.mjqzqbo.mongodb.net/?retryWrites=true&w=majority`;
 // console.log(uri)
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
